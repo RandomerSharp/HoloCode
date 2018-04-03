4d04cae02e048e5320b5e384591d7028481bc874
lcyzgdy <MyGuanDY@outlook.com>
2018-04-03 23:35:49 +0800

    Fix some error for z-test

---
+++ Assets/Script/MyMRCamera.cs
@@ -0,0 +1,14 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMRCamera : MonoBehaviour, IControllerInputHandler
+{
+    public void OnInputPositionChanged(InputPositionEventData eventData)
+    {
+        if (eventData.PressType == InteractionSourcePressInfo.Thumbstick)
+        {
+        }
+    }
+}
