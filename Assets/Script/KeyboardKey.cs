de1dd55f209411b27f8d1a9b05e0c1f06a97ae0a
lcyzgdy <MyGuanDY@outlook.com>
2018-04-11 23:50:22 +0800

    Keyboard

---
+++ Assets/Script/KeyboardKey.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class KeyboardKey : MonoBehaviour, IInputClickHandler, IFocusable
+{
+    public KeyCode key;
+    [SerializeField]
+    private Keyboard keyBoard;
+    [SerializeField]
+    private Color normalColor;
+    [SerializeField]
+    private Color focusColor;
+
+    private bool enableClick;
+
+    private void Awake()
+    {
+        GetComponent<Renderer>().material.SetColor("Color", normalColor);
+        enableClick = false;
+    }
+
+    public void OnFocusEnter()
+    {
+        GetComponent<Renderer>().material.SetColor("Color", focusColor);
+        enableClick = true;
+    }
+
+    public void OnFocusExit()
+    {
+        GetComponent<Renderer>().material.SetColor("Color", normalColor);
+        enableClick = false;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        enableClick = false;
+        if (key == KeyCode.None)
+        {
+            return;
+        }
+        keyBoard.ReceiveKey(key);
+    }
+}
